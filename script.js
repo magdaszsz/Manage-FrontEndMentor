d1fc3fce50e8c3cd615cf273682658acb074a2f4
Magda Sz <pesahson@gmail.com>
2021-06-28 20:11:35 +0200

    disable scroll

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const navToggler = document.querySelector('.nav-toggle');
+const openNav = document.querySelector('.open-menu');
+const closeNav = document.querySelector('.close-menu');
 const menu = document.querySelector('.mobile-nav');
 const body = document.querySelector('body')
 
+function noScroll() {
+  window.scrollTo(0, 0);
+}
 
-navToggler.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  body.classList.toggle('show')
+openNav.addEventListener('click', (e) => {
+  menu.classList.add('show');
+  window.addEventListener("scroll", noScroll);
 
 })
+
+closeNav.addEventListener('click', () => {
+  menu.classList.remove('show');
+  window.removeEventListener("scroll", noScroll);
+})
+
+
+
+
+
+
+
+
